c9b21b8570d55c39709f7d33e031997f38ef0da6
tripatisumit <99213688+sumittripati@users.noreply.github.com>
2025-06-02 12:52:46 +0530

    second

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log("Database Connected")
         );
-        await mongoose.connect(`${process.env.MONGODB_URI}/greencart`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/mygreencart`)
     } catch (error) {
         console.error(error.message);
     }
